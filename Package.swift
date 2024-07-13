6ecccd5bb2cd75c03087a996fbe3f326a4f34ea6
Phisto <simon.cay.gaus@gmail.com>
2024-07-14 01:30:22 +0200

    Add cxx settings

--- Package.swift
+++ Package.swift
@@ -7,20 +7,22 @@ let package = Package(
     name: "swift-taglib",
     platforms: [
         .macOS(.v13)
-    ], products: [
-        // Products define the executables and libraries a package produces, and make them visible to other packages.
+    ],
+    products: [
         .library(
             name: "TagLib",
             targets: ["TagLib"]),
-    ], targets: [
-        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
-        // Targets can depend on other targets in this package, and on products in packages this package depends on.
+    ], 
+    targets: [
         .target(
             name: "TagLib",
+            cxxSettings: [
+                .unsafeFlags(["-std=gnu++2b"]),
+            ],
             swiftSettings: [
                 .interoperabilityMode(.Cxx),
-                .unsafeFlags(["-std=gnu++2b"]),
-            ]),
+            ]
+        ),
     ],
     cxxLanguageStandard: CXXLanguageStandard.gnucxx2b
 )
